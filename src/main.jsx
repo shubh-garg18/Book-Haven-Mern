f6654990b2a2680bc0da3234cd53f416304faee4
Shubh Garg <shubh18garg82@gmail.com>
2025-01-05 03:07:53 +0530

    Include Store

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { Route, RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
+import {Provider} from 'react-redux'
+import { store } from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </Provider>,
 )
