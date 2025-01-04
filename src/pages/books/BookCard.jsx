07c280bec37685b1a0b3d9cbba5d449a41a57f16
Shubh Garg <shubh18garg82@gmail.com>
2025-01-05 03:26:40 +0530

    Add Books to Cart

--- src/pages/books/BookCard.jsx
+++ src/pages/books/BookCard.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { FiShoppingCart } from 'react-icons/fi'
-import { getImgUrl } from '../../utils/getImgUrl.js'
+import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
+import { getImgUrl } from "../../utils/getImgUrl.js";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../app/features/cart/cartSlice.js";
 
-import { Link } from'react-router-dom'
+const BookCard = ({ book }) => {
+    const dispatch = useDispatch();
 
-const BookCard = ({book}) => {
-    
+    const handleAddToCart = (book) => {
+        const item = { ...book, quantity: book.quantity || 1 };
+        dispatch(addToCart(item));
+    };
+
     return (
-        <div className=" rounded-lg transition-shadow duration-300">
-            <div
-                className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4"
-            >
+        <div className="rounded-lg transition-shadow duration-300">
+            <div className="flex flex-col sm:flex-row sm:items-center sm:h-72 sm:justify-center gap-4">
                 <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
                     <Link to={`/books/${book._id}`}>
                         <img
@@ -24,22 +29,31 @@ const BookCard = ({book}) => {
                 <div>
                     <Link to={`/books/${book._id}`}>
                         <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
-                       {book?.title}
+                            {book?.title}
                         </h3>
                     </Link>
-                    <p className="text-gray-600 mb-5">{book?.description.length > 80 ? `${book.description.slice(0, 80)}...` : book?.description}</p>
-                    <p className="font-medium mb-5">
-                        ${book?.newPrice} <span className="line-through font-normal ml-2">$ {book?.oldPrice}</span>
+                    <p className="text-gray-600 mb-5">
+                        {book?.description.length > 80
+                            ? `${book.description.slice(0, 80)}...`
+                            : book?.description}
                     </p>
-                    <button 
-                    className="btn-primary px-6 space-x-1 flex items-center gap-1 ">
-                        <FiShoppingCart className="" />
+                    <p className="font-medium mb-5">
+                        ${book?.newPrice}{" "}
+                        <span className="line-through font-normal ml-2">
+                            $ {book?.oldPrice}
+                        </span>
+                    </p>
+                    <button
+                        onClick={() => handleAddToCart(book)}
+                        className="btn-primary px-6 space-x-1 flex items-center gap-1"
+                    >
+                        <FiShoppingCart />
                         <span>Add to Cart</span>
                     </button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default BookCard
+export default BookCard;
